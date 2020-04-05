828714677eec1f42e3d2a9ec1bb78bea32b5bff1
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 12:47:29 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
   key_name               = var.key_name
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
   user_data = "${file("install_nginx.sh")}"
