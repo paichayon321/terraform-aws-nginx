6d70637835aea7b435bd46d9dd6f40e1fd22e773
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 13:19:53 +0700

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,6 +3,13 @@ sudo yum install epel-release -y
 sudo yum install nginx -y
 sudo export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 sudo export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-sudo echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.html
+#sudo echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.html
+sudo cat <<EOF> /usr/share/nginx/html/index.html
+<html>
+<body>
+<h1>Congratulations! you are success to provision nginx from terraform</h1>
+Your hostname is $HOSTNAME<br>
+</html>
+EOF
 sudo systemctl enable nginx
 sudo systemctl start nginx
