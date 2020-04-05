4ff8b5963fbb36a6d31c7eb48f3368134111bbf5
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 12:04:33 +0700

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,2 +1,8 @@
-#! /bin/bash
+#!/bin/bash
+sudo yum install epel-release -y
 sudo yum install nginx -y
+sudo export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+sudo export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+sudo echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.html
+sudo systemctl enable nginx
+sudo systemctl start nginx
